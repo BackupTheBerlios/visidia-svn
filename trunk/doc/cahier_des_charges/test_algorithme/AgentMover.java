9aa9d1c615e436e63a243c841a1e581b8dab8d12
cassou <cassou@bb92ad23-c20a-0410-a792-9a0895785805>
2006-01-23 19:56:06 +0000

    - Indentation - Simplification  de la  création d'instance  a  partir du  nom de  la classe

--- trunk/doc/cahier_des_charges/test_algorithme/AgentMover.java
+++ trunk/doc/cahier_des_charges/test_algorithme/AgentMover.java
@@ -7,58 +7,55 @@ import java.lang.reflect.Constructor;
  */
 public abstract class AgentMover {
 
-    private Agent agent;
-    private Simulator simulator;
+  private Agent agent;
+  private Simulator simulator;
 
-    public AgentMover(Agent ag, Simulator sim) {
-        this.agent = ag;
-        this.simulator = sim;
+  public AgentMover(Agent ag, Simulator sim) {
+    this.agent = ag;
+    this.simulator = sim;
+  }
+
+  public static AgentMover createAgentMover(String agentMoverClassName, 
+                                            Agent agent,
+                                            Simulator simulator) {
+    Constructor  constructor;
+    Class agClass;
+
+    try {
+      agClass = Class.forName(agentMoverClassName);
+      constructor = agClass.getConstructor(new Class []
+          {Agent.class, Simulator.class});
+
+      return (AgentMover) 
+        constructor.newInstance(new Object[]{agent, simulator});
+
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Ne peut creer l'instance !");
     }
+  }
 
-    public static AgentMover createAgentMover(String agentMoverClass, 
-                                              Agent agent,
-                                              Simulator simulator) {
-        Class [] paramsType = new Class[2];
-        Object [] params = new Object[2];
-        Constructor  constructor;
+  protected final Agent agent() {
+    return agent;
+  }
 
-        try {
-            paramsType[0] = Class.forName("Agent");
-            paramsType[1] = Class.forName("Simulator");
-
-            constructor = Class.forName(agentMoverClass).getConstructor(paramsType);
-
-            params[0] = agent;
-            params[1] = simulator;
-
-            return (AgentMover) constructor.newInstance(params);
-        }           catch           (Exception          e)           {
-            throw new IllegalArgumentException("Ne peut creer la classe !");
-        }
-    }
-
-    protected final Agent agent() {
-        return agent;
-    }
-
-    /**
-     * Deplace l'agent sur la porte suivante
-     */
-    public final void move() {
-        move(findNextDoor());
-    }
+  /**
+   * Deplace l'agent sur la porte suivante
+   */
+  public final void move() {
+    move(findNextDoor());
+  }
     
-    /**
-     * Deplace l'agent sur une porte donne
-     */
-    public final void move(int door) {
-        simulator.moveAgentTo(agent(), door);
-    }
+  /**
+   * Deplace l'agent sur une porte donne
+   */
+  public final void move(int door) {
+    simulator.moveAgentTo(agent(), door);
+  }
 
-    /** 
-     * Retourne  la porte  dans laquelle  on va  aller.  Cette methode
-     * devra etre specialisee dans les sous-classes.
-     */
-    protected abstract int findNextDoor();
+  /** 
+   * Retourne  la porte  dans laquelle  on va  aller.  Cette methode
+   * devra etre specialisee dans les sous-classes.
+   */
+  protected abstract int findNextDoor();
 
 }
