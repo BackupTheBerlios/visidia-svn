4670e3cfa125c9ad5f0dcc6c653ae0d8f26f6de1
cassou <cassou@bb92ad23-c20a-0410-a792-9a0895785805>
2006-02-21 10:03:08 +0000

    Ajout de methodes pour faciliter les tests

--- branches/premiersessai/sources/visidia/Main.java
+++ branches/premiersessai/sources/visidia/Main.java
@@ -7,15 +7,16 @@ import visidia.simulation.Simulator;
 
 public class Main {
 
-    private static SimpleGraph createGraph() {
-        SimpleGraph graph = new SimpleGraph();
+    private static SimpleGraph createBasic() {
+        SimpleGraph graph = createGraph();
 
-        graph.put(new Integer(0));
-        graph.put(new Integer(1));
-        graph.put(new Integer(2));
-        graph.put(new Integer(3));
-        graph.put(new Integer(4));
-        graph.put(new Integer(5));
+        graph.vertex(new Integer(0)).setData("BasicAgent");
+
+        return graph;
+    }
+
+    private static SimpleGraph createSynchronized() {
+        SimpleGraph graph = createGraph();
 
         graph.vertex(new Integer(0)).setData("BasicSynchronizedAgent1");
         graph.vertex(new Integer(1)).setData("BasicSynchronizedAgent1");
@@ -23,6 +24,40 @@ public class Main {
         graph.vertex(new Integer(3)).setData("BasicSynchronizedAgent3");
         graph.vertex(new Integer(4)).setData("BasicSynchronizedAgent4");
         graph.vertex(new Integer(5)).setData("BasicSynchronizedAgent4");
+
+        return graph;
+    }
+
+    private static SimpleGraph createRecognise() {
+        SimpleGraph graph;
+        Hashtable hash = new Hashtable();
+
+        hash.put("nbPassages", new Integer(0));
+
+        graph = createGraph(hash);
+
+        graph.vertex(new Integer(0)).setData("RecogniseAgent");
+
+        return graph;
+    }
+
+    private static SimpleGraph createVirus() {
+        SimpleGraph graph = createGraph();
+
+        graph.vertex(new Integer(0)).setData("Virus");
+
+        return graph;
+    }
+
+    private static SimpleGraph createGraph(Hashtable hash) {
+        SimpleGraph graph = new SimpleGraph(hash);
+
+        graph.put(new Integer(0));
+        graph.put(new Integer(1));
+        graph.put(new Integer(2));
+        graph.put(new Integer(3));
+        graph.put(new Integer(4));
+        graph.put(new Integer(5));
 
         graph.link(new Integer(0), new Integer(1));
         graph.link(new Integer(1), new Integer(2));
@@ -36,9 +71,13 @@ public class Main {
         return graph;
     }
 
+    private static SimpleGraph createGraph() {
+        return createGraph(new Hashtable());
+    }
+
     public static void main (String []args) {
 
-        SimpleGraph graph = createGraph();
+        SimpleGraph graph = createVirus();
         Simulator sim = new Simulator(graph);
         sim.startSimulation();
     }
