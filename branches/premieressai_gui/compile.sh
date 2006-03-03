6a8a25fa35cbc70fe6f963ebac0ce098f6c71df9
cassou <cassou@bb92ad23-c20a-0410-a792-9a0895785805>
2006-03-03 07:04:41 +0000

    Do not compile Main.java as it is not up to date and doesn't compile.

--- branches/premieressai_gui/compile.sh
+++ branches/premieressai_gui/compile.sh
@@ -3,6 +3,4 @@ javac -sourcepath sources -d classes \
     sources/visidia/network/*.java \
     sources/visidia/gui/presentation/userInterfaceEdition/*.java \
     sources/visidia/simulation/*.java \
-    sources/visidia/simulation/agents/*.java \
-    sources/visidia/Main.java
-
+    sources/visidia/simulation/agents/*.java
