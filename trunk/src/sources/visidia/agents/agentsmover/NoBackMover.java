4d453d61f857a8a2c5106f57b7b3eb108ce7be2f
cassou <cassou@bb92ad23-c20a-0410-a792-9a0895785805>
2006-03-17 15:33:25 +0000

    - A new mover that tries not to go to the door just seen - An agent that draws a path and then go back following the path.

---
+++ trunk/src/sources/visidia/agents/agentsmover/NoBackMover.java
@@ -0,0 +1,37 @@
+/**
+ * I'm a  linear agent mover which  try not to  go to the door  I just
+ * came from.
+ */
+package visidia.agentsmover;
+
+import visidia.simulation.agents.AgentSimulator;
+import visidia.simulation.agents.Agent;
+import visidia.simulation.agents.AgentMover;
+
+import java.util.Arrays;
+
+public class NoBackMover extends LinearAgentMover {
+    
+    public NoBackMover(Agent ag, AgentSimulator sim) {
+        super(ag, sim);
+    }
+
+    protected int findNextDoor() {
+        int doorToGo;
+
+        // Ask the LinearAgentMover for the next door
+        doorToGo = super.findNextDoor();
+
+        // Here, the  door to go is  verified not to be  from where we
+        // just came from.
+        try {
+            if (doorToGo == agent().entryDoor())
+                doorToGo = super.findNextDoor(); //ask the next door another time
+        } catch (IllegalStateException e) {
+            // It's normal to get here  for the first vertex (there is
+            // no entry door for the first vertex).
+        }
+
+        return doorToGo;
+    }
+}
