b2112e9e9f48060f6db5adfb0b5252aac0f2bee1
gautron <gautron@bb92ad23-c20a-0410-a792-9a0895785805>
2006-03-22 08:40:02 +0000

    Generic Frame to display the content of a Hashtable

---
+++ trunk/src/sources/visidia/gui/presentation/userInterfaceSimulation/HashTableFrame.java
@@ -0,0 +1,81 @@
+package visidia.gui.presentation.userInterfaceSimulation;
+
+import java.awt.BorderLayout;
+
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+
+public class HashTableFrame extends JFrame {
+
+    
+
+    public HashTableFrame(Hashtable<Object,Object> stats){
+	
+	HashTableModel tableModel;	
+	JTable resultTable;
+	JScrollPane scrollPane;
+	tableModel = new HashTableModel(stats);
+	resultTable = new JTable(tableModel);
+	scrollPane = new JScrollPane(resultTable);		
+	getContentPane().add(scrollPane,BorderLayout.CENTER);
+
+	
+    }
+    
+}
+
+
+class HashTableModel extends AbstractTableModel {
+    
+    private Vector<Object> vKeysStats;
+    private Vector<Object> vDataStats;
+    
+    public HashTableModel(Hashtable<Object,Object> hstats){
+	hashTableToVectors(hstats);
+    }
+	
+    public int getColumnCount(){
+	return 2;
+    }
+
+    public int getRowCount(){
+	return vKeysStats.size();
+    }
+
+    public Object getValueAt(int row, int col){
+	switch (col){
+	case 0: return vKeysStats.get(row);
+	case 1: return vDataStats.get(row);
+	}
+	throw new IllegalArgumentException();
+    }
+
+    private void hashTableToVectors(Hashtable<Object,Object> hstats) {
+	Set<Object> keys = hstats.keySet();
+
+	vKeysStats = new Vector(hstats.size());
+	vDataStats = new Vector(hstats.size());
+	
+	for(Object key : keys) {
+	    vKeysStats.add(key);
+	    vDataStats.add(hstats.get(key));
+	}
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
