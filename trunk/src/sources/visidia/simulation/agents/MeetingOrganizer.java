b289066dfeb7e8c336525e4f21b987a8f79e59a5
gris <gris@bb92ad23-c20a-0410-a792-9a0895785805>
2006-04-06 13:49:06 +0000

    Add of comments and some small changes

--- trunk/src/sources/visidia/simulation/agents/MeetingOrganizer.java
+++ trunk/src/sources/visidia/simulation/agents/MeetingOrganizer.java
@@ -4,12 +4,20 @@ import java.util.Collection;
 
 
 public interface MeetingOrganizer {
-    /** 
-     ** @descr 
-     ** @see whatToDoIfMeeted()
-     **/
-    public void howToMeetTogether(Collection agentsTable);
 
-    public void whatToDoIfMeeted(Collection agentsTable, Agent agentManager);
+  /* Implement the strategie of meeting between network agents
+     * @param netAgents : collection of network agents (or the set 
+     * of agents conserned by the meeting)
+     * @see whatToDoIfMeeted
+     */
+    public void howToMeetTogether(Collection netAgents);
+
+
+    /* Describe the work done during the meeting, execute agents planning on the network.
+     * @param meetedAgents : collection of synchronizedAgents who participate to the meeting.
+     * @param agentManager : The agent who manage the meeting
+     * @see visidia.simulation.agents.SynchronizedAgent.planning
+     */
+    public void whatToDoIfMeeted(Collection meetedAgents, SynchronizedAgent agentManager);
 
 }
--- trunk/src/sources/visidia/simulation/agents/SimpleMeetingOrganizer.java
+++ trunk/src/sources/visidia/simulation/agents/SimpleMeetingOrganizer.java
@@ -12,53 +12,47 @@ import java.util.Vector;
 
 public class SimpleMeetingOrganizer implements MeetingOrganizer {
   
- /**
-      * agentsForVertexId is implemented to organize meeting of agents.
-      * @return A list of on where two or more agents are meeting
-      * @deprecated ...
-      **/
-
-    // This methode implemet the way used to meet others, in a first time
-    // It doesn't change the agents list.
+    /* With this variable we can enable or not the meeting 
+     */
+    private  boolean enable = false;
+    
     public SimpleMeetingOrganizer(){
-	System.out.println("Hello, this is the organizer start...");
+	enable = true;
     }
 
 
-    public void howToMeetTogether(Collection agentsOnVertex){
-	Iterator it = agentsOnVertex.iterator();
+    /* Implement the strategie of meeting between network agents
+     * @param netAgents : collection of network agents (or the set 
+     * of agents conserned by the meeting)
+     * @see whatToDoIfMeeted
+     */
+    public void howToMeetTogether(Collection netAgents){
+	if(enable == false) return;
+	Iterator it = netAgents.iterator();
+	
 	while(it.hasNext()) {
-	    Agent agent = (SynchronizedAgent)it.next();
-	    
-	    if( agent.meet == true ){
-		System.out.println("Debut du planing de l'agent "+agent.toString());
-		if(agentsOnVertex.size() > 1){
-		    whatToDoIfMeeted(agentsOnVertex, agent);
-		}
-	    }
+	    SynchronizedAgent agent = (SynchronizedAgent)it.next();
+	    if( (agent.meet == true) && (netAgents.size() > 1))
+		whatToDoIfMeeted(netAgents, agent);
 	}
-	// 	System.out.println("Start of meeting methods ");
-	// 	if(agentsOnVertex.size() > 1){
-	// 	    whatToDoIfMeeted(agentsOnVertex);
-	// 	}
+	
     }
     
-    
-    public void whatToDoIfMeeted(Collection meetedAgents, Agent agentManager){
-	//	if(meetedAgents.size())
+    /* Describe the work done during the meeting, execute agents planning on the network.
+     * @param meetedAgents : collection of synchronizedAgents who participate to the meeting.
+     * @param agentManager : The agent who manage the meeting
+     * @see visidia.simulation.agents.SynchronizedAgent.planning
+     */
+    public void whatToDoIfMeeted(Collection meetedAgents, SynchronizedAgent agentManager){
+	if( enable == false ) return;
 	Iterator it = meetedAgents.iterator();
 	
 	while(it.hasNext()) {
-	    Agent agent = (SynchronizedAgent)it.next();
-	    
-	    //	    System.out.println("DEBUG : Debut de whatToDo...");
-	    if( agent.meet == true ){
-		if( agent != agentManager){
-		System.out.println("Debut du planing de l'agent "+agent.toString());
+	    SynchronizedAgent agent = (SynchronizedAgent)it.next();
+	    if( (agent.meet == true)  && (agent != agentManager) ){
 		agentManager.planning(agent);
-		}
 	    }
 	}
-	
     }
+    
 }
