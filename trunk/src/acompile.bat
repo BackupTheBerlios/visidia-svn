ef215362a4f2144830eab6c76e69de5c04fc51a2
bochu <bochu@bb92ad23-c20a-0410-a792-9a0895785805>
2007-02-23 16:04:47 +0000

    acompile.bat ne sert à rien compile.bat compile aussi les modules pour les agents !

--- trunk/src/acompile.bat
+++
@@ -1,1 +0,0 @@
-javac -encoding utf8 -sourcepath sources -d classes sources/visidia/algo/*.java sources/visidia/algo/synchronous/*.java sources/visidia/algoRMI/*.java sources/visidia/agents/*.java sources/visidia/agents/agentsmover/*.java sources/visidia/agents/agentchooser/*.java sources/visidia/agents/agentstats/*.java
