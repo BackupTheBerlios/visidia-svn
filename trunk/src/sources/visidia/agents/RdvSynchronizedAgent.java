2873bbe74f003abf72dd0da186830fe85d316241
bonin <bonin@bb92ad23-c20a-0410-a792-9a0895785805>
2007-03-30 20:00:51 +0000

    Algorithme du "rendez-vous" fonctionnel

--- trunk/src/sources/visidia/agents/RdvSynchronizedAgent.java
+++ trunk/src/sources/visidia/agents/RdvSynchronizedAgent.java
@@ -3,7 +3,6 @@ package visidia.agents;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Random;
-import visidia.graph.Vertex;
 import visidia.simulation.agents.Agent;
 import visidia.simulation.agents.SynchronizedAgent;
 
@@ -13,13 +12,10 @@ import visidia.simulation.agents.SynchronizedAgent;
 public class RdvSynchronizedAgent extends SynchronizedAgent {
 
 
-
 	protected void init() {
 		/* Memory of the agent that will be fusionned with the other agents */
 		this.setProperty("myName", this.toString());
 
-		/* Total number of agents on the graph */
-		int nbAllAgent = this.getSimulator().getAllAgents().size();
 
 		/* Agent move method */
 		this.setAgentMover("RandomAgentMover");
@@ -31,54 +27,45 @@ public class RdvSynchronizedAgent extends SynchronizedAgent {
 		Random rnd = new Random();
 
 
+		/* The algorithm ends when the total number of agents on the graph is equal to 1 */
+		while (this.getSimulator().getAllAgents().size() != 1) {
+			/* See all agents on the arrival's vertex */
+			int vertexId = this.getSimulator().getVertexIdentity(this);
+			//System.out.println(this.getProperty("myName")+" est sur le sommet : "+vertexId);
+			Collection agentsOnVertex = this.getSimulator().getAgentsVertexCollection(vertexId);
 
-		while (nbAllAgent != 1) {
+
+			/* Make the union of the agents' memories if current agent has a better ID */
+			if (! agentsOnVertex.isEmpty() ){
+				int agentId = this.getIdentity();
+				String agentName = (String) this.getProperty("myName");
+				Iterator it = agentsOnVertex.iterator();
+
+
+				while (it.hasNext()){
+					Agent currentAgent = (Agent) it.next();	
+					int currentId = currentAgent.getIdentity();
+					String currentAgentName = (String) currentAgent.getProperty("myName");
+
+
+					/* Save the agents'name and kill the agent if better ID */
+					if (agentId > currentId){
+						this.setProperty("myName", agentName + " kills ( " + currentAgentName + " )");
+						this.getSimulator().realyKillAgent(currentAgent);
+						//System.out.println("L'agent "+agentName+" a tué l'agent "+currentAgentName);
+					}
+				}
+			}
 
 			/* Move the agent to a next vertex with a chance of 1 out of 3 */
 			int choice = rnd.nextInt() % 2;
-			System.out.println(this.getProperty("myName")+" a tiré : "+choice);
+			//System.out.println(this.getProperty("myName")+" a tiré : "+choice);
 
-			this.nextPulse();
 			if (choice == 0){
 				this.move();
-				this.nextPulse();
-
-				/* See all agents on the arrival's vertex */
-				Vertex vertexArrival = this.getSimulator().getVertexArrival(this);
-				System.out.println(vertexArrival+" : "+this.getSimulator().getVertexIdentity(this)+ this.getProperty("myName"));
-				Collection agentsOnVertex = vertexArrival.getAgentsNames();
-
-
-				/* Make the union of the agents' memories if current agent has a better ID */
-				if (! agentsOnVertex.isEmpty() ){
-					System.out.println("Il y a plusieurs agents sur un sommet");
-					int id = this.getIdentity();
-					String myName = (String) this.getProperty("myName");
-					Iterator it = agentsOnVertex.iterator();
-
-
-					while (it.hasNext()){
-						Agent currentAgent = (Agent) it.next();	
-						int currentId = currentAgent.getIdentity();
-						String currentName = (String) currentAgent.getProperty("myName");
-
-
-						/* Save the agents'name and kill the agent */
-						if (id > currentId){
-							this.setProperty("myName", myName + currentName);
-							this.getSimulator().killAgent(currentAgent);
-						}
-					}
-				}
-				this.nextPulse();
 			}
-			else{
-				this.nextPulse();
-				this.nextPulse();
-			}
+			this.nextPulse();
+
 		}
 	}
-
-
 }
-
